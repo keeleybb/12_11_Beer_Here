39c7b833e94b1bfb2881041806325b55f81c537e
Heidi Einholz <heidein5@gmail.com>
2019-12-07 11:45:39 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+$(document).ready(function(){
+      //initialize carousel
+  $(".carousel").carousel()
+
+     //initialize carousel full slider
+  $(".carousel.carousel-slider").carousel({fullWidth:true});
+
+//initialize slider
+  $(".slider").slider();
+
+  });
+
