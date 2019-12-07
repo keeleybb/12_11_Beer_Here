b50407b2fd1b8899c85fd4a92b222e05a3b0667a
kbb <54555434+keeleybb@users.noreply.github.com>
2019-12-07 12:29:09 -0700

    Trying to find my code

--- script.js
+++ script.js
@@ -1,33 +1,13 @@
-
-
-  // Or with jQuery
-
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
-
-$(document).ready(function(){
-      //initialize carousel
-  $(".carousel").carousel()
-
-     //initialize carousel full slider
-  $(".carousel.carousel-slider").carousel({fullWidth:true});
-
-//initialize slider
-  $(".slider").slider();
-
-  });
-
+var map;
 
 function initMap() {
   // The location of Uluru
   var uluru = { lat: 39.7392, lng: -104.9903 };
   // The map, centered at Uluru
-  var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 12,
     center: uluru
   });
   // The marker, positioned at Uluru
   var marker = new google.maps.Marker({ position: uluru, map: map });
 }
-
